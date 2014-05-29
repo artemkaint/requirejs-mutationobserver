be415353275784f6c7c33e851166c314b50e7fe1
artemkaint <artemkaint@nodasoft.com>
2014-05-29 12:34:07 +0400

    UMD loader

--- requirejs-mutationobserver.js
+++ requirejs-mutationobserver.js
@@ -1,13 +1,20 @@
-define([
-  'WeakMap',
-  'MutationObserver',
-  './lib/jquery.mutationobserver'
-], function(WeakMap, MutationObserver, JQueryMutationObserver){
+(function (root, factory) {
+  if (typeof exports === 'object') {
+    // CommonJS
+    module.exports = factory(require('WeakMap', 'MutationObserver', './lib/jquery.mutationobserver'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['WeakMap', 'MutationObserver', './lib/jquery.mutationobserver'], factory);
+  } else {
+    // Global Variables
+    root.returnExportsGlobal = factory(root.WeakMap, root.MutationObserver, root.JQueryMutationobserver);
+  }
+}(this, function (WeakMap, MutationObserver, JQueryMutationobserver) {
   if(window.MutationObserver) {
     return window.MutationObserver;
   } else if(window.MutationEvent) {
     return MutationObserver;
   } else {
-    return JQueryMutationObserver;
+    return JQueryMutationobserver;
   }
-});
+}));
